8d237dee544e00afccd43ab8ca40628063b5b59a
mtseet <melekam@gmail.com>
2025-07-25 18:56:01 +0300

    return docker script to original

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -6,9 +6,5 @@ mkdir build
 cp -a ../dist build
 
 docker build . --tag s3-object-manager:latest
-docker build . -t ghcr.io/mtseet/s3-object-manager:latest
-docker build . -t ghcr.io/mtseet/s3-object-manager:1.0.1             
-echo docker push ghcr.io/mtseet/s3-object-manager:latest
-echo docker push ghcr.io/mtseet/s3-object-manager:1.0.1
 
 rm -rf build
